75b8e1e432a86bea568a42cb9912dddab37c0517
CHunjat <minuszz@naver.com>
2025-02-14 17:43:00 +0900

    2025.02.14.(수정) (금)

--- C/CSTUDY/helloworld.cpp
+++ C/CSTUDY/helloworld.cpp
@@ -6,10 +6,10 @@
 주 제   : HELLO WOLRD 출력
 */
 
-#include <stdio.h>
+#include <stdio.h> // stdio.h 를 포함시켜라 그럼 stdio.h 를 뭘까 -> stdio.h 표준화된 기능들?
 
-int main()
-{
-	printf("hello world!");
+int main() // int는 무엇일까? int는 하나의 정의된 문법으로 정수(소수가아닌 숫자) , main은 c언의 시작점
+{ //인트 메인의 내용 중 
+	printf("hello world!"); //printf는 프린트해라? 그럼 f는 뭘까 ? ("내용") 을 ;이 문장을 마친다. 
 	return 0;
-}
+} // 
