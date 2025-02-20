9ad58b00101cef1df90a115cbea1802649ba8d8b
CHunjat <minuszz@naver.com>
2025-02-20 17:50:15 +0900

    20250220. fri

--- C/Operator/main.c
+++ C/Operator/main.c
@@ -120,15 +120,25 @@ int main()
 	// 게임의 스토리
 
 	//무기 강화하는데
+	// 강사님 스타일.
+	// int basepower = 10;
+	// int weapon = 1;
+	// int weight = 2;
+	// int attackpower = basepower + weaponlv *  weight;
+	// 
+	// printf("무기의 레벨 : %d, 무기의 공격력 : %d/n;", weaponlv++
+	// 
+	// 
+	// 
+	// 
+	// 
 	//n 강일때의 무기, n+1 무기의 정보를 화면에 출력 스토리 작성
 
 	int 롱소드;
 	int 기본공격력 = 10;
 	int 무기레벨 = 1;
 	int 강화레벨 = 1;
-	int 무기공격력;
-
-	무기공격력 = 기본공격력 + 무기레벨 * 강화레벨;
+	int 무기공격력 = 기본공격력 + 무기레벨 * 강화레벨;
 
 	    printf("강화 전\n");
 		printf("이름 : 롱소드\n");
@@ -151,17 +161,37 @@ int main()
 
 		printf("강화에 성공하였습니다\n");
 
-		printf("강화 후 롱소드 : 기본공격력%d\n, 무기레벨%d\n, 강화레벨%d\n,무기공격력%d\n", 기본공격력+1, 무기레벨+1, 강화레벨+1, 무기공격력);
+		printf("강화 후 롱소드 : 기본공격력%d\n, 무기레벨%d\n, 강화레벨%d\n,무기공격력%d\n", 기본공격력++, 무기레벨++, 강화레벨++, 무기공격력);
 
 
 
 
 
-	// 게임 스토리2.
-	// 아이템 인벤토리, slot, 기타 아이템 수집 stack x 갯수
-	// slot 최대 갯수 : 변수
-	// stack 최대 갯수 : 변수
-	// a아이템으,n개 획득 , b 아이템 m개 획득
-	//현재 인벤토리의 갯수가 가득 찼는지 안찼는지 표현하는 결과를 표현해보세요.
-	//3항 연산자
+		// 게임의 스토리2.
+
+		// 아이템 인벤토리.  Slot. 기타 아이템 수집. Stack x 갯수.
+		// Slot 최대 갯수 : 변수
+		// Stack 최대 갯수 : 변수
+		// A 아이템을 n개 획득, B 아이템 m개 획득
+		// 현재 인벤토리의 갯수가 가득 찼는지 안찼는지 표현하는 결과를 표현해보세요.
+
+		int maxInventorySlot = 10;
+		int AStackCount = 200;
+		int BStackCount = 200;
+		int ADrop = 555;
+		int BDrop = 7777;
+
+		printf("1시간 동안 사냥을 해서 아이템을 획득을 했다.\n");
+		printf("A아이템 : %d, B아이템 : %d 획득했다.\n", ADrop, BDrop);
+
+		// 나머지
+
+		int AInventoryCount = (ADrop % AStackCount) == 0 ? (ADrop / AStackCount) : (ADrop / AStackCount) + 1;
+		int BInventoryCount = (BDrop % BStackCount) == 0 ? (BDrop / BStackCount) : (BDrop / BStackCount) + 1;
+
+		printf(" A의 갯수 :%d B의 갯수 : %d\n", AInventoryCount, BInventoryCount);
+		printf("결과 :  %s\n", maxInventorySlot <= (AInventoryCount + BInventoryCount) ? "가득참" : "가득안참");
+		 
 }
+
+
